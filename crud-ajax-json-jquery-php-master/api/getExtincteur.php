cd9d489584afca544acd7b10e6580746e09da4b2
KhalidBoughaba <boughaba.khalid.dev@gmail.com>
2021-05-24 16:14:35 +0100

    del pass root

--- crud-ajax-json-jquery-php-master/api/getExtincteur.php
+++ crud-ajax-json-jquery-php-master/api/getExtincteur.php
@@ -1,5 +1,5 @@
 <?php
-$dbh = new PDO("mysql:host=localhost;dbname=produit_extincteur","root","Khalid167995");
+$dbh = new PDO("mysql:host=localhost;dbname=produit_extincteur","root","root");
 $sql = " SELECT * FROM extincteur ";
 $extincteurQuery = $dbh->query($sql);
 $getExtincteur = $extincteurQuery->fetchAll(PDO::FETCH_ASSOC);
