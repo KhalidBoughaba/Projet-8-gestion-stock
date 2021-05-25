3c1c6cba00cd5a1f52a1875b8b854bb9e7e0a6e8
KhalidBoughaba <boughaba.khalid.dev@gmail.com>
2021-05-25 13:24:36 +0100

    modified css border radius li

--- crud-ajax-json-jquery-php-master/api/getExtincteur.php
+++ crud-ajax-json-jquery-php-master/api/getExtincteur.php
@@ -1,5 +1,5 @@
 <?php
-$dbh = new PDO("mysql:host=localhost;dbname=produit_extincteur","root","root");
+$dbh = new PDO("mysql:host=localhost;dbname=produit_extincteur","root","Khalid167995");
 $sql = " SELECT * FROM extincteur ";
 $extincteurQuery = $dbh->query($sql);
 $getExtincteur = $extincteurQuery->fetchAll(PDO::FETCH_ASSOC);
